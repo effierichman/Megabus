921d5b3647f45645c349d2b21156daa67b7383e1
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-02-14 16:27:47 -0500

    backend file structure

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
