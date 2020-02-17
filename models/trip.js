5b824d0940ee3c2a5a528415174fddedd8f6025a
effierichman <effierichman@gmail.com>
2020-02-17 10:21:24 -0500

    updated seeds and trip model

--- models/trip.js
+++ models/trip.js
@@ -7,8 +7,13 @@ module.exports = (sequelize, DataTypes) => {
     departure_time: DataTypes.STRING,
     arrival_time: DataTypes.STRING,
     travel_date_begin: DataTypes.STRING,
-    travel_date_end: DataTypes.STRING
-  }, {});
+    travel_date_end: DataTypes.STRING,
+    paid: DataTypes.BOOLEAN,
+    departure_long: DataTypes.FLOAT,
+    departure_latt: DataTypes.FLOAT,
+    arrival_long: DataTypes.FLOAT,
+    arrival_latt: DataTypes.FLOAT
+    }, {});
   Trip.associate = function(models) {
     // associations can be defined here
   };
