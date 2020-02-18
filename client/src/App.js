97004e72f1382b8a47641706293985b610a8327b
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-02-18 10:41:55 -0500

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Route, withRouter } from 'react-router-dom'
 import './App.css';
 import Header from './components/header'
 import BookForm from './components/bookForm'
+import BookTrips from './components/bookTrips'
 
 function App() {
   return (
     <React.Fragment>
           <Header />
-          <BookForm />
-
-    
-
+          <Route exact path="/" component={BookForm} />
+          <Route exact path="/trips" component={BookTrips} />
      </React.Fragment>
   );
 }
---
+++ client/src/components/bookTrips.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class BookTrips extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      trips: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios(`http://localhost:3000/trips/boston/nyc/11:00am`)
+      this.setState({ trips: response.data.selectedTrip })
+      console.log(response.data.selectedTrip )
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  render() {
+    const trips = this.state.trips.map(trip => (
+      <div key={trip.id}>
+        <h2>
+          {trip.departure_city} {trip.destination_city}
+          {/* <button
+            onClick={() => this.props.history.push(`/users/${user.id}/items`)}
+          >
+            View Items
+          </button>
+          <button
+            onClick={() =>
+              this.props.history.push(`/users/${user.id}/create-item`)
+            }
+          >
+            Create Item
+          </button> */}
+        </h2>
+      </div>
+    ))
+
+    return (
+        <div>
+        <h4>Trips</h4>
+        <ul>{trips}</ul>
+        </div>
+    
+    )
+  }
+}
+
+export default BookTrips
+  
