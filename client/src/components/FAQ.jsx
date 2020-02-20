09974664165eaa786db5ad858f57135efe94e8cd
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-02-20 16:33:38 -0500

    styling

--- client/src/components/FAQ.jsx
+++ client/src/components/FAQ.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function FAQ (){
     return(
-    <div>
+    <div className="faq-container">
         <h1 className="notification-email">Help & Frequently Asked Questions</h1>
-        <h3 className="FROM">Help Topics</h3>
+        <h3 className="topic">Help Topics</h3>
         <button className="Button-help">Email us</button>
         <button className="Button-help">Reservations</button>
         <button className="Button-help">Travelling on the bus</button>
 
-        <h3 className="FROM">FAQ</h3>
+        <h3 className="topic">FAQ</h3>
         <button className="Button-help">What is Megabus RIDE?</button>
         <button className="Button-help">What can I bring with me?</button>
         <button className="Button-help">What if I lost an item on the bus?</button>
