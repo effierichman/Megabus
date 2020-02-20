3363cbdb90e6b2b2728bcea9597ad376b8d5a153
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-02-19 19:23:46 -0500

    hamburger menu icon

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,14 +5,23 @@ import {Route, BrowserRouter as Router} from 'react-router-dom'
 
 const Header = () => (
   <div className= "container">
-  <NavLink style={{ textDecoration: 'none' }}
+     <nav className="TOP-Header">
+
+       {/* //hey donald this is the hamberger bar */}
+       <div className="hamberger-container">
+         <div className="hamberger"></div>
+          <div className="hamberger"></div>
+         <div className="hamberger"></div>
+    </div>
+
+    <NavLink style={{ textDecoration: 'none' }}
 to = '/'>
-    <nav className="TOP-Header">
     <h1 className="megabuscom">megabus.com</h1>
-      
+    </NavLink>
+    
     </nav>
    
-    </NavLink>
+  
     </div>
   )
   
